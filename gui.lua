9618432a5a40f88eea019b1910488a8b4396c2c2
Leoc76101111 <a@a.a>
2025-02-23 21:35:06 +1100

    added option to use dungeon reset instead for pit/horde

--- gui.lua
+++ gui.lua
@@ -1,5 +1,5 @@
 local plugin_label = 'lazarus_pit'
-local plugin_version = 'v0.0.1'
+local plugin_version = 'v0.0.2'
 
 local gui = {}
 
@@ -11,13 +11,15 @@ gui.elements = {
     main_tree = tree_node:new(0),
     main_toggle = create_checkbox(false, 'main_toggle'),
     escape_percentage = slider_int:new(10, 100, 40, get_hash("escape_percentage")),
+    dungeon_reset = create_checkbox(true, 'dungeon_reset'),
 }
 
 function gui.render()
     if not gui.elements.main_tree:push('Lazarus Pit | Leoric | ' .. plugin_version) then return end
     gui.elements.main_toggle:render("Enable", "Enable the bot")
     if gui.elements.main_toggle:get() then
-        gui.elements.escape_percentage:render("Health %%", "%% health to immediately use scroll")
+        gui.elements.escape_percentage:render("Health %%", "%% health to immediately use scroll or dungeon reset")
+        gui.elements.dungeon_reset:render("Use Dungeon Reset for Pit/Horde", "Use Dungeon Reset for Pit/Horde to save scrolls")
     end
     gui.elements.main_tree:pop()
 end
--- main.lua
+++ main.lua
@@ -5,34 +5,41 @@ local local_player
 local function update_locals()
     local_player = get_local_player()
 end
+local function player_in_dungeon()
+    local current_zone = get_current_world():get_current_zone_name()
+    -- if player in pit or horde, can add nmd next time
+    return current_zone == "EGD_MSWK_World_02" or
+        current_zone == "EGD_MSWK_World_01" or
+        current_zone == "S05_BSK_Prototype02"
+end
 
 local function main_pulse()
     local settings = {
         enabled = gui.elements.main_toggle:get(),
-        escape_percentage = gui.elements.escape_percentage:get()
+        escape_percentage = gui.elements.escape_percentage:get(),
+        dungeon_reset = gui.elements.dungeon_reset:get(),
     }
     if not local_player or not settings.enabled then return end
     local player_current_health = local_player:get_current_health();
     local player_max_health = local_player:get_max_health();
     local health_percentage = player_current_health / player_max_health;
-    if health_percentage <=  (settings.escape_percentage / 100) then
-        local consumeable_items = local_player:get_consumable_items()
-        for _,item in pairs(consumeable_items) do
-            -- sno_id for scroll of escape is 598884
-            if item:get_sno_id() == 598884 then
-                loot_manager.use_item(item)
-                return
+    if health_percentage <= (settings.escape_percentage / 100) then
+        if player_in_dungeon() and settings.dungeon_reset then
+            reset_all_dungeons()
+        else
+            local consumeable_items = local_player:get_consumable_items()
+            for _,item in pairs(consumeable_items) do
+                -- sno_id for scroll of escape is 598884
+                if item:get_sno_id() == 598884 then
+                    loot_manager.use_item(item)
+                    return
+                end
             end
         end
     end
 end
 
 local function render_pulse()
-    local settings = {
-        enabled = gui.elements.main_toggle:get(),
-        escape_percentage = gui.elements.escape_percentage:get()
-    }
-    if not local_player or not settings.enabled then return end
 end
 
 on_update(function()
